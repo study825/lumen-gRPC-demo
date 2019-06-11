c79d451e1580e1c72b4722cd6f321af6468d0a2f
CalmLi <liqiang@xinchanedu.com>
2019-06-11 14:38:52 +0800

    增加protoc php

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -2,17 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Service\SnowService;
+
 class ExampleController extends Controller
 {
+    protected $snow;
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(
+        SnowService $snow
+    )
     {
-        //
+        $this->snow = $snow;
     }
 
-    //
+
+    public function snow()
+    {
+        return $this->snow->getSnow();
+    }
 }
---
+++ app/Services/SnowService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+/**
+ *  雪花算法service
+ */
+class SnowService
+{
+	public function getSnow()
+	{
+		
+	} 
+}
